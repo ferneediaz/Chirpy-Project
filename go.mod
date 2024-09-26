14a57516d73c2693192ca75274aa5552e834ead6
ferneediaz <dandiaz10@yahoo.com>
2024-09-27 02:17:16 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ferneediaz/Chirpy
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const filepathRoot = "./app"
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", healthHandler)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
